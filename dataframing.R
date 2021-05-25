9b91d8f253434acf20ec0fb6d8c83ae6078c9f1e
pallu06 <84726808+pallu06@users.noreply.github.com>
2021-05-25 13:03:01 +0530

    Add files via upload

---
+++ dataframing.R
@@ -0,0 +1,26 @@
+df <- data.frame(x= 1:3, y= c('a','b','c'))
+df
+df[1]
+df[1,]
+df[1,1]
+df[c(1,3),2]
+df[c(1,3),]
+df[c(1,3),1]
+df1 <- data.frame(x=1:5, name = c('pallavi', 'varun','nisha','kranthi','family'))
+df1                
+df1[c(1,2),]
+df1[c(2)]
+ 
+ df1[c(1)]
+ df1[c(1,3),2]
+ df1[1,]
+ df1[2,]
+ df1[c(1,4),c(2)]
+ df1[3,c(1,2)]
+ df1[c(1,3),1]
+ df1[c(1,3),3]
+ df1[c(1,4),1]
+ df1[c(3),1]
+ df1[c(2),2]
+ df1[c(1,2),1]
+ 
